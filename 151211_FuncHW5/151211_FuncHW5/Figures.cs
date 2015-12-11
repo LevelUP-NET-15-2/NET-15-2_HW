0b7161bb87c6d99c1e56fa1168c1f46597b5ba24
Rostislav Bigdan <beardedmanul@gmail.com>
2015-12-11 17:49:02 +0200

    Add hw 5

---
+++ 151211_FuncHW5/151211_FuncHW5/Figures.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace _151211_FuncHW5
+{
+    class Figures
+    {
+        /// <summary>
+        /// The structure that defines the Point
+        /// </summary>
+        public struct Point
+        {
+            public double _x;
+            public double _y;
+
+            public override string ToString()
+            {
+                return string.Format("({0},{1})", _x, _y);
+            }
+        }
+
+        /// <summary>
+        /// The structure that defines the Circle
+        /// </summary>
+        public struct Circle
+        {
+            public Point _p;
+            public double _r;
+            public double _a;
+
+            public override string ToString()
+            {
+                return string.Format("Circle: \n\tCenter: {0} \n\tRadius: {1} \n\tArea: {2}", _p, _r, _a);
+            }
+        }
+
+        /// <summary>
+        /// The structure that defines the Triangle
+        /// </summary>
+        public struct Triangle
+        {
+            public Point _p1;
+            public Point _p2;
+            public Point _p3;
+            public double _ab;
+            public double _bc;
+            public double _ac;
+            public double _per;
+            public double _halfper;
+            public double _area;
+
+            public override string ToString()
+            {
+                return string.Format("Triangle: \n\tPoint 1: {0} \n\tPoint 2: {1} \n\tPoint 3: {2} \n\tAB: {3} \n\tBC: {4} \n\tAC: {5} \n\tPerimeter: {6} \n\tHalf perimeter: {7} \n\tArea: {8}"
+                    ,_p1, _p2, _p3, _ab, _bc, _ac, _per, _halfper, _area);
+            }
+        }
+
+        /// <summary>
+        /// The structure that defines the Triangle in Circle
+        /// </summary>
+        public struct Triangle_in_Circle
+        {
+            public Circle circ;
+            public Triangle tri;
+
+            public Triangle_in_Circle(Circle c, Triangle t)
+            {
+                circ = c;
+                tri = t;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0} \nin \n{1}", tri, circ);
+            }
+        }
+
+        public Figures(Point a, Point b, Point c, out Triangle_in_Circle tic)
+        {
+            Circle c1 = new Circle();
+            Triangle t1 = new Triangle();
+            
+
+            t1._p1._x = a._x;
+            t1._p1._y = a._y;
+
+            t1._p2._x = b._x;
+            t1._p2._y = b._y;
+
+            t1._p3._x = c._x;
+            t1._p3._y = c._y;
+
+            t1._bc = Math.Sqrt(Math.Pow(t1._p3._x - t1._p2._x, 2) + Math.Pow(t1._p3._y - t1._p2._y, 2));
+            t1._ab = Math.Sqrt(Math.Pow(t1._p2._x - t1._p1._x, 2) + Math.Pow(t1._p2._y - t1._p1._y, 2));
+            t1._ac = Math.Sqrt(Math.Pow(t1._p3._x - t1._p1._x, 2) + Math.Pow(t1._p3._y - t1._p1._y, 2));
+
+            t1._per = t1._ab + t1._bc + t1._ac;
+            t1._halfper = 0.5 * t1._per;
+            t1._area = Math.Sqrt(t1._halfper * (t1._halfper - t1._ab) * (t1._halfper - t1._bc) * (t1._halfper - t1._ac));
+
+            c1._r = (t1._ab * t1._bc * t1._ac) / (4.0 * Math.Sqrt(t1._halfper * (t1._halfper - t1._ab) * (t1._halfper - t1._bc) * (t1._halfper - t1._ac)));
+            c1._a = Math.PI * Math.Pow(c1._r, 2.0);
+
+            c1._p._x = -((((t1._p1._y - t1._p2._y) * (Math.Pow(t1._p3._x, 2.0) + Math.Pow(t1._p3._y, 2.0))) + ((t1._p2._y - t1._p3._y)
+                * (Math.Pow(t1._p1._x, 2.0) + Math.Pow(t1._p1._y, 2.0))) + ((t1._p3._y - t1._p1._y) * (Math.Pow(t1._p2._x, 2.0) + Math.Pow(t1._p2._y, 2.0))))
+                / (2.0 * (((t1._p1._x - t1._p2._x) * (t1._p3._y - t1._p1._y)) - ((t1._p1._y - t1._p2._y) * (t1._p3._x - t1._p1._x)))));
+
+            c1._p._y = -((((t1._p1._x - t1._p2._x) * (Math.Pow(t1._p3._x, 2) + Math.Pow(t1._p3._y, 2.0))) + ((t1._p2._x - t1._p3._x) 
+                * (Math.Pow(t1._p1._x, 2.0) + Math.Pow(t1._p1._y, 2.0))) + ((t1._p3._x - t1._p1._x) * (Math.Pow(t1._p2._x, 2) + Math.Pow(t1._p2._y, 2.0)))) 
+                / (2.0 * (((t1._p1._x - t1._p2._x) * (t1._p3._y - t1._p1._y)) - ((t1._p1._y - t1._p2._y) * (t1._p3._x - t1._p1._x)))));
+
+            Triangle_in_Circle tic1 = new Triangle_in_Circle(c1, t1);
+            tic = tic1;
+        }
+    }
+}
---
+++ 151211_FuncHW5/151211_FuncHW5/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace _151211_FuncHW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+
+            Console.WriteLine("Aloha! Это таки пятая домашняя работа Бигдана Ростислава!");
+            Console.WriteLine("Пришла пора сделать выбор: ");
+            Console.WriteLine("1) Если вы таки хотите работать с фигурами введите 1");
+            Console.WriteLine("2) Если вы таки хотите работать с расписанием введите 2");
+
+            Int32.TryParse(Console.ReadLine(), out choice);
+
+            if (choice == 1)
+            {
+                Figures.Point a = new Figures.Point();
+                Figures.Point b = new Figures.Point();
+                Figures.Point c = new Figures.Point();
+
+
+                Console.Clear();
+                Console.WriteLine("Вы выбрали работу с фигурами");
+
+                Console.WriteLine("Приготовьтесь вводить координаты точек треугольника");
+                Console.ReadKey();
+
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.Clear();
+
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Введите X-координату первой точки: ");
+                        double.TryParse(Console.ReadLine(), out a._x);
+                    }
+
+                    if (i == 1)
+                    {
+                        Console.WriteLine("Введите Y-координату первой точки: ");
+                        double.TryParse(Console.ReadLine(), out a._y);
+                    }
+
+                    if (i == 2)
+                    {
+                        Console.WriteLine("Введите X-координату второй точки: ");
+                        double.TryParse(Console.ReadLine(), out b._x);
+                    }
+
+                    if (i == 3)
+                    {
+                        Console.WriteLine("Введите Y-координату второй точки: ");
+                        double.TryParse(Console.ReadLine(), out b._y);
+                    }
+
+                    if (i == 4)
+                    {
+                        Console.WriteLine("Введите X-координату третьей точки: ");
+                        double.TryParse(Console.ReadLine(), out c._x);
+                    }
+
+                    if (i == 5)
+                    {
+                        Console.WriteLine("Введите Y-координату третьей точки: ");
+                        double.TryParse(Console.ReadLine(), out c._y);
+                    }
+                }
+                Console.Clear();
+
+                Figures.Triangle_in_Circle tic = new Figures.Triangle_in_Circle();
+
+                Figures f = new Figures(a, b, c, out tic);
+
+                Console.WriteLine(tic);
+               
+                Console.ReadKey();
+            }
+
+            if (choice == 2)
+            {
+                char[] delimiterChars = { ' ', ',', '.' };
+
+                Console.WriteLine("В какие дни у вас есть занятия?");
+                string days_source = Console.ReadLine();
+                string days = days_source.ToUpper();
+
+                string[] dayssp = days.Split(delimiterChars);
+
+                Schedule sc = new Schedule(dayssp);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
