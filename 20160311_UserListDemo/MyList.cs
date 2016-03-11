bf424f94ad9276c85aedff060cb58d20207b9f95
Rostislav Bigdan <beardedmanul@gmail.com>
2016-03-11 20:59:51 +0200

    Add HW

---
+++ 20160311_UserListDemo/MyList.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20160305_UserListDemo
+{
+    public class MyList
+    {
+        // Элемент списка
+        private class MyElem
+        {
+            public int Info    // Информационная часть
+            {
+                get;
+                set;
+            }
+
+            public MyElem Next    // Ссылка на следующий элемент в списке
+            {
+                get;
+                set;
+            }
+        }
+
+        // Добавление в конец списка
+        public void AddToEnd(int val)
+        {
+            MyElem newElem = new MyElem() { Info = val, Next = null };    // новый элемент списка
+
+            if (_first == null)
+            {
+                _first = newElem;    // новый элемент становится 1-м в списке
+            }
+            else
+            {
+                MyElem currElem = _first;
+
+                // проходии до последнего элемента
+                while (currElem.Next != null)
+                {
+                    currElem = currElem.Next;    // переход к следующему элементу в списке
+                }
+
+                currElem.Next = newElem;    // новый элемент стновится последним в списке
+            }
+        }
+
+        public void AddToBegin(int val)
+        {
+            MyElem newElem = new MyElem() { Info = val, Next = _first };    // новый элемент списка
+
+            _first = newElem;
+        }
+
+        // Проверка списка на пустоту
+        public bool IsEmpty()
+        {
+            return _first == null;
+        }
+
+        // Проверка на существование элемента в списке
+        public bool IsExist(int info)
+        {
+            MyElem currElem = _first;
+
+            while (currElem != null)
+            {
+                if (currElem.Info == info)    // элемент в списке найден
+                {
+                    return true;
+                }
+
+                currElem = currElem.Next;    // переход к следующему элементу в списке
+            }
+
+            return false;    // Элемент не найден
+        }
+
+        public int GetFirst()
+        {
+            if (_first==null)
+            {
+                throw new Exception("Список пуст");
+            }
+
+            int retVal = _first.Info;
+
+            _first = _first.Next;
+
+            return retVal;
+        }
+
+
+        public int GetLast()
+        {
+            if (_first == null)
+            {
+                throw new Exception("Список пуст");
+            }
+
+            if (_first.Next == null)   // Список состоит из одного элемента
+	        {
+		        int retVal = _first.Info;
+
+                _first = null;
+
+                return retVal;
+	        }
+
+            MyElem currElem = _first;
+
+            // Находим предпоследний элемент
+            while (currElem.Next.Next != null)
+            {
+                currElem = currElem.Next;
+            }
+
+            int retVal1 = currElem.Next.Info;
+
+            currElem.Next = null;  // предпоследний элемент становится последним
+
+            return retVal1;
+        }
+
+        /// <summary>
+        /// Метод, возвращающий элемент по заданному индексу
+        /// </summary>
+        /// <param name="val">Индекс элемента</param>
+        /// <returns>Значение элемента</returns>
+        public int GetElemByPos(int val)
+        {
+            if (_first == null)
+            {
+                throw new Exception("Список пуст");
+            }
+            else
+            {
+                MyElem currElem = _first;
+
+                int i = 0;
+
+                // проходим до последнего элемента
+                while (currElem.Next != null)
+                {
+                    currElem = currElem.Next;
+                    ++i;
+
+                    if (i == val)
+                    {
+                        return currElem.Info;
+                    }
+                }
+            }
+
+            throw new Exception("Список не содержит элемента с указанным индексом!");
+        }
+
+        /// <summary>
+        /// Индексатор, основанный на базе GetElemByPos
+        /// </summary>
+        /// <param name="index">Индекс элемента</param>
+        /// <returns>Значение элемента</returns>
+        public int this[int index]
+        {
+            get
+            {
+                return GetElemByPos(index);
+            }
+        }
+
+        /// <summary>
+        /// Свойство, возвращающее длину списка
+        /// </summary>
+        /// <returns>Длина списка</returns>
+        public int Length()
+        {
+            if (_first == null)
+            {
+                return 0;
+            }
+            else
+            {
+                MyElem currElem = _first;
+
+                int i = 1;
+
+                // проходии до последнего элемента
+                while (currElem.Next != null)
+                {
+                    i++;
+                    currElem = currElem.Next;
+                }
+
+                return i;
+            }
+        }
+
+        /// <summary>
+        /// Нахождение количества вхождений указанного элемента
+        /// </summary>
+        /// <param name="elem">Искомый элемент</param>
+        /// <returns>Количество вхождений искомого элемента</returns>
+        public int GetAmountOfElems(int elem)
+        {
+            if (_first == null)
+            {
+                throw new Exception("Список пуст!");
+            }
+            else
+            {
+                MyElem currElem = _first;
+
+                int i = 0;
+
+                // проходии до последнего элемента
+                while (currElem.Next != null)
+                {
+                    if (currElem.Info == elem)
+                    {
+                        ++i;
+                    }
+
+                    currElem = currElem.Next;
+                }
+
+                return i;
+            }
+        }
+
+        public void DelElem(int elem)
+        {
+            if (_first == null)
+            {
+                throw new Exception("Список пуст!");
+            }
+            else
+            {
+                MyElem currElem = _first;
+
+                //if (currElem.Next == null && elem == 0)
+                //{
+                //    currElem = null;
+                //}
+
+                int i = 0;
+
+                // проходии до предпоследнего элемента
+                while (currElem.Next != null)
+                {
+                    MyElem temp = null;
+
+                    temp = currElem;
+
+                    if (i == elem)
+                    {
+                        currElem.Next = currElem.Next.Next;
+                        i++;
+                    }
+                    else
+                    {
+                        currElem = currElem.Next;
+                        i++;
+                    }
+
+                }
+            }
+        }
+
+        // HW:
+
+        // 1. GetElemByPos - Нахождение элемента по позиции   +
+        // 2. Реализовать индексатор на основе GetElemByPos   +
+        // 3. Length - свойство, возвращающее длину списка    +
+        // 4. Нахождение количества вхождений указанного элемента   +
+        // 5. Удаление элемента, находящегося на указанной позиции   +-
+        // 6. Удаление указанного элемента (по значению)   ?
+
+        // 7. Добавить класс для пользовательских исключений MyListException + продумать и предусмотреть различные варианты его использования   -
+
+        // 8. Добавить в класс работу с событиями (элемент добавлен, элемен удалён и т.д.)   -
+
+        //  *** 9. Boxing/unboxing (int Info  ---> object Info)   -
+
+        private MyElem _first = null;    // изначально список пуст
+    }
+}
---
+++ 20160311_UserListDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20160305_UserListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList lst = new MyList();
+
+            lst.AddToEnd(10);
+            lst.AddToEnd(20);
+            lst.AddToEnd(30);
+
+
+            Console.WriteLine(lst.IsExist(30));
+            Console.WriteLine(lst.IsExist(60));
+
+
+            try
+            {
+                Console.WriteLine(lst.GetFirst());
+                Console.WriteLine(lst.GetFirst());
+                Console.WriteLine(lst.GetFirst());
+                Console.WriteLine(lst.GetFirst());
+                Console.WriteLine(lst.GetFirst());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            MyList lst1 = new MyList();
+
+            lst1.AddToEnd(110);
+            lst1.AddToEnd(120);
+            lst1.AddToEnd(130);
+
+            Console.WriteLine(lst1.GetElemByPos(2));
+
+            Console.WriteLine(lst1[1]);
+
+            Console.WriteLine(lst1.Length());
+
+            Console.WriteLine(lst1.GetAmountOfElems(220));
+
+            lst1.DelElem(1);
+
+            Console.WriteLine(lst1.Length());
+        }
+    }
+}
